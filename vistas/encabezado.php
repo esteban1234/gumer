4df1fc5ce7774b6f4e39ca6ea28cedc5d2a984f2
esteban1234 <tva_libra@hotmail.com>
2018-04-11 09:48:54 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="f-0efCjXOqMa7xKfl0e7bVUpKyhS9tQ9PwEuAIFFMAA" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>GUMER SEGUROS</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
--- vistas/envia.php
+++ vistas/envia.php
@@ -22,8 +22,7 @@ $mail->From = $correo;
 $mail->FromName = $nombre;
 
 //To address and name
-// $mail->addAddress("info@segurosgumer.com");
-$mail->addAddress("ballina.santiago@gmail.com");
+$mail->addAddress("info@segurosgumer.com");
 
 
 
